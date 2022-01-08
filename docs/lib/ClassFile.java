79691b84e38d321da563be79fe93d0da793098d4
yufarui <yufaruigo@gmail.com>
2022-01-08 15:45:56 +0800

    文档修改

---
+++ docs/lib/ClassFile.java
@@ -0,0 +1,9 @@
+/**
+ * 此类 辅助查看 类的结构
+ */
+public class ClassFile {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/indi/yufr/jvm/example/ClassFile.java
@@ -0,0 +1,11 @@
+package indi.yufr.jvm.example;
+
+/**
+ * 此类 辅助查看 类的结构
+ */
+public class ClassFile {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
