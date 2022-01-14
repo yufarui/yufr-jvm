957da554a555f4f7d1e47b0b943b80273c6ed433
farui.yu <farui.yu@kingstarfintech.com>
2022-01-14 17:23:02 +0800

    更新文档

--- src/main/java/indi/yufr/jvm/example/MainClass.java
+++ src/main/java/indi/yufr/jvm/example/MainClass.java
@@ -3,7 +3,7 @@ package indi.yufr.jvm.example;
 /**
  * 此类 辅助查看 类的结构
  */
-public class ClassFile {
+public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
