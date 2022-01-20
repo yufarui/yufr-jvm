5501cdcfad2ff8f4e9f1638225c3822646a49a83
farui.yu <farui.yu@kingstarfintech.com>
2022-01-20 16:34:51 +0800

    更新文档

--- src/main/java/indi/yufr/jvm/share/vm/memory/MetaspaceObj.java
+++
@@ -1,9 +0,0 @@
-package indi.yufr.jvm.share.vm.memory;
-
-/**
- * @Author: ziya
- * @Date: 2021/3/26 11:36
- */
-public class MetaspaceObj extends AllocatedObj {
-
-}
--- src/main/java/indi/yufr/jvm/share/vm/runtime/VFrame.java
+++
@@ -1,9 +0,0 @@
-package indi.yufr.jvm.share.vm.runtime;
-
-/**
- * Created By ziya
- * QQ: 3039277701
- * 2021/3/29
- */
-public class VFrame {
-}
